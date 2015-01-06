dbce0897f19e7767df0e6988279d2cc2862a9952
Joachim Metz <joachim.metz@gmail.com>
2015-01-06 08:08:09 +0100

    Applied updates.

--- fsclfstools/fsclfsinfo.c
+++ fsclfstools/fsclfsinfo.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from a Common Log File System (CLFS) store
  *
- * Copyright (C) 2010-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
