211a995ebd48f8e7f785308f5dad01f67bfa6ad1
Joachim Metz <joachim.metz@gmail.com>
2019-10-15 05:21:26 -0700

    Applied updates

---
+++ .travis/script.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Script to run script step on Travis-CI
+#
+# Version: 20190808
+
+# Exit on error.
+set -e;
+
+if test ${TRAVIS_OS_NAME} = "linux" || test ${TRAVIS_OS_NAME} = "linux-ppc64le";
+then
+	export PATH=$(echo $PATH | tr ":" "\n" | sed '/\/opt\/python/d' | tr "\n" ":" | sed "s/::/:/g");
+fi
+
+if test ${TARGET} = "docker";
+then
+	docker run -t -v "${PWD}:/libfsclfs" ${DOCKERHUB_REPO}:${DOCKERHUB_TAG} sh -c "cd libfsclfs && .travis/script_docker.sh";
+
+elif test ${TARGET} != "coverity";
+then
+	.travis/runtests.sh;
+
+	if test ${TARGET} = "macos-gcc-pkgbuild";
+	then
+		export VERSION=`sed '5!d; s/^ \[//;s/\],$//' configure.ac`;
+
+		make install DESTDIR=${PWD}/osx-pkg;
+		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libfsclfs;
+		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libfsclfs;
+
+		pkgbuild --root osx-pkg --identifier com.github.libyal.libfsclfs --version ${VERSION} --ownership recommended ../libfsclfs-${VERSION}.pkg;
+	fi
+fi
+
--- tests/fsclfs_test_memory.c
+++ tests/fsclfs_test_memory.c
@@ -35,15 +35,15 @@
 
 #if defined( HAVE_FSCLFS_TEST_MEMORY )
 
-static void *(*fsclfs_test_real_malloc)(size_t)                 = NULL;
-static void *(*fsclfs_test_real_memcpy)(void *, void *, size_t) = NULL;
-static void *(*fsclfs_test_real_memset)(void *, int, size_t)    = NULL;
-static void *(*fsclfs_test_real_realloc)(void *, size_t)        = NULL;
+static void *(*fsclfs_test_real_malloc)(size_t)                       = NULL;
+static void *(*fsclfs_test_real_memcpy)(void *, const void *, size_t) = NULL;
+static void *(*fsclfs_test_real_memset)(void *, int, size_t)          = NULL;
+static void *(*fsclfs_test_real_realloc)(void *, size_t)              = NULL;
 
-int fsclfs_test_malloc_attempts_before_fail                     = -1;
-int fsclfs_test_memcpy_attempts_before_fail                     = -1;
-int fsclfs_test_memset_attempts_before_fail                     = -1;
-int fsclfs_test_realloc_attempts_before_fail                    = -1;
+int fsclfs_test_malloc_attempts_before_fail                           = -1;
+int fsclfs_test_memcpy_attempts_before_fail                           = -1;
+int fsclfs_test_memset_attempts_before_fail                           = -1;
+int fsclfs_test_realloc_attempts_before_fail                          = -1;
 
 /* Custom malloc for testing memory error cases
  * Note this function might fail if compiled with optimation
@@ -82,7 +82,7 @@ void *malloc(
  */
 void *memcpy(
        void *destination,
-       void *source,
+       const void *source,
        size_t size )
 {
 	if( fsclfs_test_real_memcpy == NULL )
