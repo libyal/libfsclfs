d5b7af1a176d2c573e2486ad10843256aaeeedc5
Joachim Metz <joachim.metz@gmail.com>
2026-06-21 16:01:03 +0200

    Worked on tests and changes for CI tests

--- include/libfsclfs/error.h
+++ include/libfsclfs/error.h
@@ -82,7 +82,7 @@ enum LIBFSCLFS_ARGUMENT_ERROR
 	 */
 	LIBFSCLFS_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBFSCLFS_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBFSCLFS_OUTPUT_ERROR
 {
 	LIBFSCLFS_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBFSCLFS_OUTPUT_ERROR_INSUFFICIENT_SPACE	= 1
 };
